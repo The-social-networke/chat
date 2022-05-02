b3f8b57a091fdfa059cd85714a3cfbd8a1f86fe9
vadim <vadim.skuratovskij@gmail.com>
2022-05-02 18:36:36 +0300

    try to fix socket cors

--- src/main/java/com/socialnetwork/chat/config/WebSocketConfig.java
+++ src/main/java/com/socialnetwork/chat/config/WebSocketConfig.java
@@ -29,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
             //.setAllowedOriginPatterns("*")
-            .setAllowedOrigins("**")
+            .setAllowedOrigins("http://localhost:8081")
             .withSockJS();
     }
 
