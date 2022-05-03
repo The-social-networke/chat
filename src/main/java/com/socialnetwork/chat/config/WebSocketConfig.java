abadae939d71fb41964a5eeefac4a9178cd93252
vadim <vadim.skuratovskij@gmail.com>
2022-05-03 12:18:12 +0300

    try to fix socket cors

--- src/main/java/com/socialnetwork/chat/config/WebSocketConfig.java
+++ src/main/java/com/socialnetwork/chat/config/WebSocketConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -27,12 +26,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-            .setAllowedOrigins("*")
+            .setAllowedOriginPatterns("*")
             .withSockJS();
+        registry.addEndpoint("/ws-chat")
+            .setAllowedOriginPatterns("*");
     }
 
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        //registration.interceptors(new UserInterceptor(url));
-    }
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        //registration.interceptors(new UserInterceptor(url));
+//    }
 }
