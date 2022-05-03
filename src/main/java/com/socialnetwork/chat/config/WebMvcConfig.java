ab268e2088ecc2e3b6481e5431b70e4b28c9182f
vadim <vadim.skuratovskij@gmail.com>
2022-05-03 14:12:41 +0300

    try to fix socket cors

--- src/main/java/com/socialnetwork/chat/config/WebMvcConfig.java
+++ src/main/java/com/socialnetwork/chat/config/WebMvcConfig.java
@@ -3,8 +3,6 @@ package com.socialnetwork.chat.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,7 +11,7 @@ import java.util.List;
 
 @Configuration
 @EnableSpringDataWebSupport
-class WebMvcConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
@@ -31,12 +29,6 @@ class WebMvcConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigu
 //        source.registerCorsConfiguration("/**", configuration);
 //        return source;
 //    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
-    }
-
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
